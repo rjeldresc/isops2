68034d2a1541b12a08f89edb5aece41386465623
Rodrigo Jeldres Carrasco <rodrigo.jeldres@gmail.com>
2022-04-30 18:41:03 -0400

    correccion de errores

--- isops2/Program.cs
+++ isops2/Program.cs
@@ -1,47 +1,62 @@
-﻿using System;
+﻿using DiscUtils.Iso9660;
+using System;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using DiscUtils.Iso9660;
 
 namespace isops2
 {
     internal class Program
     {
         static void Main(string[] args)
-        {            
-            if (args.Length == 1)
-            {
-                String pathIso = args[0];
-                using (FileStream isoRom = File.Open(pathIso, FileMode.Open, FileAccess.Read))
-                {
-                    string romId = null;
-                    byte[] magicNumberArray = new byte[11];
-                    isoRom.Position = 65881;
-                    isoRom.Read(magicNumberArray, 0, 11);
-                    String magicNumberString = Encoding.ASCII.GetString(magicNumberArray);
-                    isoRom.Position = 0;
-                    if (magicNumberString != "PLAYSTATION")
-                        Console.WriteLine("No es iso de ps2 :c");
+        {
+            switch(args.Length)
+            {                
+                case 1:
+                    string pathIso = args[0];
+                    string extension = Path.GetExtension(pathIso).ToLower();
+                    if (extension == ".iso")
+                    {
+                        using (FileStream isoRom = File.Open(pathIso, FileMode.Open, FileAccess.Read))
+                        {
+                            string romId = null;
+                            byte[] magicNumberArray = new byte[11];
+                            isoRom.Position = 65881;
+                            isoRom.Read(magicNumberArray, 0, 11);
+                            string magicNumberString = Encoding.ASCII.GetString(magicNumberArray);
+                            isoRom.Position = 0;
+                            if (magicNumberString == "PLAYSTATION")
+                            {
+                                var regex = new Regex(@"[A-Z]{4}_[0-9]+.[0-9]+");
+                                CDReader reader = new(isoRom, true);
+                                var id = reader.Root.GetFiles()
+                                    .Select(file => file.FullName)
+                                    .Where(file => regex.IsMatch(file))
+                                    .First();
+                                int posicion = id.IndexOf(";");
+                                romId = id.Substring(0, posicion);
+                                isoRom.Close();
+                                System.IO.File.Move(pathIso, Path.GetDirectoryName(pathIso) + "\\" + romId + "." + Path.GetFileName(pathIso));
+                                Console.WriteLine("RomId: {0}", romId);
+                                Console.WriteLine(".iso renombrada correctamente");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error: No es .iso de Ps2 :c");
+                                isoRom.Close();
+                            }
+                        }
+                    }
                     else
                     {
-                        var regex = new Regex(@"[A-Z]{4}_[0-9]+.[0-9]+");
-                        CDReader reader = new(isoRom, true);
-                        var id = reader.Root.GetFiles()
-                            .Select(file => file.FullName)
-                            .Where(file => regex.IsMatch(file))
-                            .First();
-                        int posicion = id.IndexOf(";");
-                        romId = id.Substring(0, posicion);                     
-                        Console.WriteLine("RomId: {0}", romId);
+                        Console.WriteLine("Error: No se define archivo .iso");
                     }
-                    isoRom.Close();
-                    System.IO.File.Move(pathIso, Path.GetDirectoryName(pathIso) + "\\" + romId + "." + Path.GetFileName(pathIso));
-                }
+                    break;
+                default:
+                    Console.WriteLine("Error: No se ingresaron argumentos");
+                    break;
             }
-            else
-                Console.WriteLine("error: no se define archivo iso");
         }
     }
 }
